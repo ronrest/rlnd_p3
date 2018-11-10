605878ab23c3fbcafeb35f50f3eac3fa307e130b
ronrest <ronny.coding@gmail.com>
2018-11-10 23:29:30 +1100

    FEAT: Add Hard Update

--- support.py
+++ support.py
@@ -2,3 +2,13 @@ def tensorfy(x):
     """ Converts an array-like object to a pytorch float tensor """
     return torch.Tensor(x).float()
 
+# https://github.com/ikostrikov/pytorch-ddpg-naf/blob/master/ddpg.py#L15
+def hard_update(target, source):
+    """
+    Copy network parameters from source to target
+    Inputs:
+        target (torch.nn.Module): Net to copy parameters to
+        source (torch.nn.Module): Net whose parameters to copy
+    """
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)

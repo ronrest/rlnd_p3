3ca33af0bbcff68d286aa62a9e8145414f406692
ronrest <ronny.coding@gmail.com>
2018-11-10 23:45:43 +1100

    FEAT: Add DDPGAgent class

--- ddpg.py
+++ ddpg.py
@@ -1,5 +1,10 @@
 import numpy as np
 import torch
+from network import Network
+from support import hard_update
+from torch.optim import Adam
+
+device = 'cpu'
 class OUNoise:
     def __init__(self, action_dimension, scale=0.1, mu=0, theta=0.15, sigma=0.2):
         self.action_dimension = action_dimension
@@ -19,3 +24,34 @@ class OUNoise:
         self.state = x + dx
         return torch.tensor(self.state * self.scale).float()
 
+
+class DDPGAgent:
+    def __init__(self, actor_layer_sizes=[24, 128,128,2], critic_layer_sizes=[24, 128,128,1], lr_actor=1.0e-2, lr_critic=1.0e-2, logger=None):
+        super(DDPGAgent, self).__init__()
+
+        self.actor = Network(layer_sizes=actor_layer_sizes, actor=True, logger=logger).to(device)
+        self.critic = Network(layer_sizes=critic_layer_sizes, logger=logger).to(device)
+        self.target_actor = Network(layer_sizes=actor_layer_sizes, actor=True, logger=logger).to(device)
+        self.target_critic = Network(layer_sizes=critic_layer_sizes, logger=logger).to(device)
+
+        self.noise = OUNoise(actor_layer_sizes[-1], scale=1.0 )
+
+        # initialize targets same as original networks
+        hard_update(self.target_actor, self.actor)
+        hard_update(self.target_critic, self.critic)
+
+        self.actor_optimizer = Adam(self.actor.parameters(), lr=lr_actor)
+        self.critic_optimizer = Adam(self.critic.parameters(), lr=lr_critic, weight_decay=0.0)
+
+
+    def act(self, obs, noise=0.0):
+        obs = torch.tensor(obs, dtype=torch.float)
+        obs = obs.to(device)
+        action = self.actor(obs) + noise*self.noise.noise()
+        return action
+
+    def target_act(self, obs, noise=0.0):
+        obs = torch.tensor(obs, dtype=torch.float)
+        obs = obs.to(device)
+        action = self.target_actor(obs) + noise*self.noise.noise()
+        return action

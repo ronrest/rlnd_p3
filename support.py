27556584a340d2e1303cc2582e2a7fee56a9eacf
ronrest <ronny.coding@gmail.com>
2018-11-11 12:30:55 +1100

    FEAT: Add random swap of experiences

--- support.py
+++ support.py
@@ -1,7 +1,28 @@
+import numpy as np
+import torch
+
 def tensorfy(x):
     """ Converts an array-like object to a pytorch float tensor """
     return torch.Tensor(x).float()
 
+def random_swap(x):
+    """ Randomly swaps the first two rows of data in an array-like object
+    """
+    if np.random.randint(2) == 1:
+        return x[[1,0]]
+    else:
+        return x
+
+def random_swap_agent_experiences(experiences):
+    """ Randomly swaps the experiences of agent1/agent2
+    """
+    (agents_states, global_state, actions, rewards, next_agents_states, next_global_state, dones) = experiences
+    agents_states = random_swap(agents_states)
+    actions = random_swap(actions)
+    rewards = random_swap(rewards)
+    next_agents_states = random_swap(next_agents_states)
+    dones = random_swap(dones)
+    return (agents_states, global_state, actions, rewards, next_agents_states, next_global_state, dones)
 
 
 # https://github.com/ikostrikov/pytorch-ddpg-naf/blob/master/ddpg.py#L11

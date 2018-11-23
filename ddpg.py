75a69db0e22fe9e726cc1f8d5e14712f7388c8f2
ronrest <ronny.coding@gmail.com>
2018-11-24 08:19:14 +1100

    FEAT: changed default argument values to DDPGAgent()

--- ddpg.py
+++ ddpg.py
@@ -1,21 +1,22 @@
 import numpy as np
 import torch
+from torch.optim import Adam
 from network import Network
 from support import hard_update
-from torch.optim import Adam
 from OUNoise import OUNoise
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class DDPGAgent:
-    def __init__(self, actor_layer_sizes=[24, 128,128,2], critic_layer_sizes=[24, 128,128,1], lr_actor=1.0e-2, lr_critic=1.0e-2, clamp_actions=True, logger=None):
+    """ Implements the structure of the DDPG Reinforcement Learning algorithm"""
+    def __init__(self, actor_layer_sizes=[24, 128,128,2], critic_layer_sizes=[24, 128,128,1], lr_actor=1e-3, lr_critic=1e-3, clamp_actions=True, logger=None, log_layers=False):
         super(DDPGAgent, self).__init__()
 
         # SET UP ACTOR AND CRITIC NETWORKS
-        self.actor = Network(layer_sizes=actor_layer_sizes, actor=True, logger=logger).to(device)
-        self.critic = Network(layer_sizes=critic_layer_sizes, logger=logger).to(device)
-        self.target_actor = Network(layer_sizes=actor_layer_sizes, actor=True, logger=logger).to(device)
-        self.target_critic = Network(layer_sizes=critic_layer_sizes, logger=logger).to(device)
+        self.actor = Network(layer_sizes=actor_layer_sizes, actor=True, logger=logger, log_layers=log_layers).to(device)
+        self.critic = Network(layer_sizes=critic_layer_sizes, logger=logger, log_layers=log_layers).to(device)
+        self.target_actor = Network(layer_sizes=actor_layer_sizes, actor=True, logger=logger, log_layers=log_layers).to(device)
+        self.target_critic = Network(layer_sizes=critic_layer_sizes, logger=logger, log_layers=log_layers).to(device)
 
         # INITIALIZE TARGET NETWORKS TO HAVE SAME WEIGHTS AS LOCAL NETWORKS
         hard_update(self.target_actor, self.actor)
@@ -28,7 +29,6 @@ class DDPGAgent:
         # NOISE - for exploration of actions
         self.noise = OUNoise(actor_layer_sizes[-1], scale=1.0 )
         self.clamp_actions = clamp_actions
-
 
     def act(self, obs, noise=0.0):
         """ Given a tensor representing the states, it returns the predicted

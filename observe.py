7fe7c422c20fed0b0fe4a78d703cdec6e7366041
ronrest <ronny.coding@gmail.com>
2018-11-24 13:28:10 +1100

    FEAT: update observe.py

--- observe.py
+++ observe.py
@@ -1,138 +1,135 @@
-import time
+#!/usr/bin/env python
+# coding: utf-8
 import os
+from collections import deque
 
 import numpy as np
 import torch
-# from tensorboardX import SummaryWriter
-
 from unityagents import UnityEnvironment
+
 from maddpg import MADDPG
-
-
-from support import scale_agent_state
 from support import tensorfy
-# from support import maybe_make_dir, pretty_time
+import time
 
+# ##############################################################################
+#                                                  SETTINGS
+# ##############################################################################
+MODEL_NAME = "model_g"
+ENV_FILE = "Tennis_Linux/Tennis.x86_64"
+# ENV_FILE = "Tennis_Linux_NoVis/Tennis.x86_64"
 
-UNITY_ENV_FILE = "Tennis_Linux/Tennis.x86_64"
-MODEL_NAME = "model_k"
-seed = 45
+ACTOR_LAYER_SIZES =  [12, 256,128,2]
+CRITIC_LAYER_SIZES = [22, 256,128,1]
+
+N_EPISODES = 5    # Number of episodes of game play to observe
+
+SEED = 777
+CLAMP_ACTIONS=True
+
+N_AGENTS = 2       # Number of agents in environment
+N_ACTIONS = 2      # Number of actions the agent can take
+MAX_STEPS = 5000   # max number of steps for an episode
+
+FRAME_DELAY = 0.0  # amount of time to pause between frames
 
 # ##############################################################################
 #                                                  SUPPORT
 # ##############################################################################
-def seeding(seed=1):
-    """ Sets random seeds"""
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-
-
 def process_agent_states(states):
-    """ Given an array of shape [n_agents, state_size], it scales
-        the features to keep all data within approx the range -1 to 1
-    """
-    states = scale_agent_state(states)
-    return states[:,:]
-
+    """ Given an array of shape [n_agents, state_size], it
+        Only keeps a subset of the state information for each agent """
+    # states = scale_agent_state(states)
+    return states[:,[3,4,5,11,12,13,16,17,18,19,20,21]]
 
 def process_gobal_state(states):
     """ Given an array of shape [n_agents, state_size], it
-        concatenates the agents features to create a flat array
-        of shape [stae_size*n_agents]
+        Only keeps a subset of the state information for each agent
+        that is not duplicate, or redundant.
+        returns an array of shape [18]
     """
-    states = scale_agent_state(states)
-    a = states[0,:]
-    b = states[1,:]
+    # states = scale_agent_state(states)
+    a = states[0,[3,11,16,17,18,19]]
+    b = states[1,[3,4,5,11,12,13,16,17,18,19,20,21]]
     c = np.concatenate([a,b])
     return c
-
 
 # ##############################################################################
 #                                                  SETUP
 # ##############################################################################
-seeding(seed)
+# Generate Output Directory Paths
+model_dir = os.path.join("models", MODEL_NAME)
+snapshots_dir = os.path.join(model_dir, "snapshots")
+
+# SET SEEDS FOR REPRODUCIBILITY
+np.random.seed(SEED)
+torch.manual_seed(SEED)
 
 
 # ##############################################################################
 #                                                  ENVIRONMENT
 # ##############################################################################
-env = UnityEnvironment(file_name=UNITY_ENV_FILE)
+env = UnityEnvironment(file_name=ENV_FILE, seed=SEED)
 
 # get the default brain
 brain_name = env.brain_names[0]
 brain = env.brains[brain_name]
 
-# reset the environment
-env_info = env.reset(train_mode=True)[brain_name]
-states = env_info.vector_observations
-
-# calculate dimenstions needed for agent network
-agent_state_size = process_agent_states(states).shape[1]
-global_state_size = process_gobal_state(states).shape[0]
 
 # ##############################################################################
 #                                                  AGENT
 # ##############################################################################
-N_ACTIONS = 2
-N_AGENTS = 2
-maddpg = MADDPG(
-            actor_layer_sizes=[agent_state_size, 256,256,2],
-            critic_layer_sizes=[global_state_size + (N_ACTIONS*N_AGENTS), 256,256,1],
-            )
-
-maddpg.load_model(os.path.join("models", MODEL_NAME, "snapshots", "best_model.params"))
+# INITIALIZE AGENT, AND LOAD WEIGHTS FROM BEST SNAPSHOT
+maddpg = MADDPG(actor_layer_sizes=ACTOR_LAYER_SIZES,
+                critic_layer_sizes=CRITIC_LAYER_SIZES,
+                clamp_actions=CLAMP_ACTIONS,
+                logger=None,
+                )
+maddpg.load_model(os.path.join(snapshots_dir, "best_model.snapshot"))
 
 
 # ##############################################################################
-#                                                  PLAY WITH ENVIRONMENT
+#                                                 INTERACT WITH ENVIRONMENT
 # ##############################################################################
-MAX_STEPS = 1000
-n_episodes = 1
-# t0 = time.time()
-frame_delay = 0.01 # amount ot time to pause before moving on to next frame
+for episode_i in range(1, N_EPISODES+1):
+    print("{dec}\nEpisode {i}\n{dec}\n".format(dec="="*60, i=episode_i))
+    # INITIALIZE FOR NEW EPISODE
+    rewards_this_episode = np.zeros((N_AGENTS,))
+    env_info = env.reset(train_mode=False)[brain_name]
+    states = process_agent_states(env_info.vector_observations)
+    global_state = process_gobal_state(env_info.vector_observations)
 
-for episode_i in range(n_episodes):
-    # RESET ENVIRONMENT
-    env_info = env.reset(train_mode=True)[brain_name] # reset the environment
-    states = env_info.vector_observations         # get next state (for each agent)
-    agents_states = process_agent_states(states)
-    global_state = process_gobal_state(states)
-    scores = np.zeros(2)
-    maddpg.reset_noise()
-    # COLLECT A FULL EPISODE OF EXPERIENCES
-    # step = 0
+    # ITERATE THROUGH EACH STEP OF AN EPISODE
     for step in range(MAX_STEPS):
-        # GET ACTIONS TO TAKE FROM AGENT
-        # Given agent states, get the actions for each agent from the actor policy
-        # (and some random noise for exploration)
-        # - then convert list of action tensors to a 2D numpy array
-        #   [n_agents, n_actions]
-        actions = maddpg.act(tensorfy(agents_states))
-        actions = torch.stack(actions).detach().numpy()
+        # GET ACTIONS TO TAK ADN INTERACT WITH THE ENVIRONMENT
+        actions = maddpg.act(tensorfy(states), stacked=True)
+        env_info = env.step(actions)[brain_name]
 
-        # INTERACT WITH THE ENVIRONMENT
-        env_info = env.step(actions)[brain_name]           # send all actions to tne environment
-        next_states = env_info.vector_observations         # get next state (for each agent)
-        rewards = env_info.rewards                         # get reward (for each agent)
-        dones = env_info.local_done                        # see if episode finished
-        scores += env_info.rewards                         # update the score (for each agent)
-        if np.any(dones):                                  # exit loop if episode finished
-            print("DOne in {} steps".format(step))
-            print(scores)
-            break
-
-        # PROCESS STATES
-        next_agents_states = process_agent_states(next_states)
-
-        # PREPARE FOR NEXT STEP
-        # - update state and decay the noise
-        agents_states = next_agents_states
+        # EXTRACT AND PROCESS THE RETURNED VALUES FROM ENVIRONMENT
+        next_states = process_agent_states(env_info.vector_observations)
+        next_global_state = process_gobal_state(env_info.vector_observations)
+        rewards = env_info.rewards
+        dones = env_info.local_done
 
         # UPDATE REWARDS
-        # rewards_this_episode += rewards
+        rewards_this_episode += rewards
 
-        #print(actions.reshape([1,-1]))
-        time.sleep(frame_delay)
+        # PREPARE FOR NEXT TIMESTEP
+        states = next_states
+        global_state = next_global_state
 
+        # END EPISODE IF ANY AGENT IS DONE
+        if any(dones):
+            print("Episode complete!")
+            break
+
+        # PAUSE BETEWEEN FRAMES
+        time.sleep(FRAME_DELAY)
+
+    if not any(dones):
+        print("Reached max mumber of steps")
+
+    # FEEDBACK PRINTOUT - the score for this episode
+    agg_reward_this_episode = np.max(rewards_this_episode)
+    print("Episode Score: ", agg_reward_this_episode)
 
 env.close()
